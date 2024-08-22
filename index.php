db0f76791e59bf0b4e9b590bf68ab5a1153dba9f
Bernhard Leers <bl@wamoco.de>
2024-08-22 13:06:50 +0200

    wip

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
  * See COPYING.txt for license details.
  */
 namespace Magento\Backend\Controller\Adminhtml\Auth;
-
+sdfads
 class DeniedIframe extends \Magento\Backend\Controller\Adminhtml\Auth
 {
     /**
